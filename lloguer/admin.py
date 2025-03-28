5801aaa972e1ba6d39efe3e27f5e4b3b81e784f9
Unai Muñoz <unaimunoz2024@gmail.com>
2025-03-28 15:48:02 +0100

    Fita 2

--- lloguer/admin.py
+++ lloguer/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Automobil
 
-# Register your models here.
+@admin.register(Automobil)
+class AutomobilAdmin(admin.ModelAdmin):
+    # Especificar qué campos se mostrarán en el listado de objetos
+    list_display = ('matricula', 'marca', 'model')
+    
+    # Opcional: añadir campos de búsqueda
+    search_fields = ('matricula', 'marca', 'model')
